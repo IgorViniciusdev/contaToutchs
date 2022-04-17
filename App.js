32891aa7c82bac481ffdd857453ac3704ebafa52
= <igor.offn@gmail.com>
2022-04-17 19:21:34 -0300

    Contador de toutch de usuário

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
-export default function App() {
+export default function countToutch() {
+  const [count, setCount] = useState(0); 
+  console.log("Clicou " + count + " vezes")
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <View>
+        <Text>Você cliclou no botão: {count} vezes</Text>
+      </View>
+      <View>
+        <Button title="Toque aqui" onPress = {() => setCount(count + 1)}/>
+      </View>
     </View>
   );
+}
+
+function App(){
+<countToutch/>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#611d91',
     alignItems: 'center',
     justifyContent: 'center',
   },
